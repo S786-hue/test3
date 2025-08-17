21685c600b85dcbc07a6394106e13043af029706
S786-hue.safiullahrayeen@gmail.com <safiullahrayeen@gmail.com>
2025-08-17 19:43:42 +0530

    First Commit

---
+++ test-psa/src/main/java/com/psa/TestPsaApplication.java
@@ -0,0 +1,14 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaApplication {
+
+	// hkjhkjhkhkj
+	public static void main(String[] args) {
+		SpringApplication.run(TestPsaApplication.class, args);
+	}
+
+}
